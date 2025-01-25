49b359ceb2ea138a254a18c48f244282ff9ca0f8
leeys9423 <leeys9423@gmail.com>
2025-01-26 08:25:32 +0900

    백준: 연결 요소의 개수

---
+++ youngsub/java/P11724.java
@@ -0,0 +1,63 @@
+// bfs - 연결 요소의 개수
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class P11724 {
+
+    static ArrayList<Integer>[] arr;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[N + 1];
+        arr = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            arr[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            arr[x].add(y);
+            arr[y].add(x);
+        }
+
+        int count = 0;
+
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                bfs(i);
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+    private static void bfs(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(start);
+        visited[start] = true;
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            for (int next : arr[current]) {
+                if (!visited[next]) {
+                    visited[next] = true;
+                    queue.offer(next);
+                }
+            }
+        }
+    }
+}
