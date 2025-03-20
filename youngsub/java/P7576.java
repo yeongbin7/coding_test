3e538e4f5e6d6dfabdc749a4e406ae142250e90e
leeys9423 <leeys9423@gmail.com>
2025-03-20 09:27:07 +0900

    백준: 토마토

---
+++ youngsub/java/P7576.java
@@ -0,0 +1,84 @@
+// 토마토
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class P7576 {
+
+    static int N, M;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    static class Point {
+        int x;
+        int y;
+        int day;
+
+        public Point(int x, int y, int day) {
+            this.x = x;
+            this.y = y;
+            this.day = day;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+        visited = new boolean[N][M];
+        Queue<Point> queue = new LinkedList<>();
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if (map[i][j] == 1) {
+                    queue.offer(new Point(i, j, 0));
+                    visited[i][j] = true;
+                }
+            }
+        }
+
+        int answer = 0;
+
+        while (!queue.isEmpty()) {
+            Point current = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = current.x + dx[i];
+                int ny = current.y + dy[i];
+
+                if (nx < 0 || nx >= N || ny < 0 || ny >= M) {
+                    continue;
+                }
+
+                if (map[nx][ny] == 0 && !visited[nx][ny]) {
+                    visited[nx][ny] = true;
+                    queue.offer(new Point(nx, ny, current.day + 1));
+                    answer = Math.max(answer, current.day + 1);
+                }
+            }
+        }
+
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (!visited[i][j] && map[i][j] == 0) {
+                    answer = -1;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
