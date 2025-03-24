a6be33ee263576dd9e50f7fbd7122dcc6e1bafe9
leeys9423 <leeys9423@gmail.com>
2025-03-24 09:30:09 +0900

    백준: 제로

--- youngsub/java/stack/P10773.java
+++ youngsub/java/stack/P10773.java
@@ -1,1 +1,28 @@
-// 제로 - 실버
+// 제로 - 실버4
+import java.util.Scanner;
+import java.util.Stack;
+
+public class P10773 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int K = sc.nextInt();
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < K; i++) {
+            int number = sc.nextInt();
+            if (number == 0) {
+                stack.pop();
+            } else {
+                stack.push(number);
+            }
+        }
+
+        int answer = 0;
+        while (!stack.isEmpty()) {
+            answer += stack.pop();
+        }
+
+        System.out.println(answer);
+    }
+}
