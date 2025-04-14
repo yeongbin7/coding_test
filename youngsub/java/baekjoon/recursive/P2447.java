df31a3ce53dc547f26d5a9a8b1fabcd2e20aa3c0
leeys9423 <leeys9423@gmail.com>
2025-04-14 20:42:25 +0900

    백준 - 별 찍기

--- youngsub/java/baekjoon/recursive/P2447.java
+++ youngsub/java/baekjoon/recursive/P2447.java
@@ -1,3 +1,48 @@
 // 별 찍기 - 10 - 골드 5
+import java.util.*;
+
 public class P2447 {
+
+    static char[][] arr;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        arr = new char[N][N];
+
+        for (int i = 0; i < N; i++) {
+            Arrays.fill(arr[i], ' ');
+        }
+
+        recursive(N, 0, 0);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    static void recursive(int size, int row, int col) {
+        if (size == 3) {
+            for (int i = row; i < row + 3; i++) {
+                for (int j = col; j < col + 3; j++) {
+                    if (i != row + 1|| j != col + 1) {
+                        arr[i][j] = '*';
+                    }
+                }
+            }
+            return;
+        }
+
+        int newSize = size / 3;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == 1 && j == 1) {
+                    continue;
+                }
+                recursive(newSize, row + i * newSize, col + j * newSize);
+            }
+        }
+    }
 }
